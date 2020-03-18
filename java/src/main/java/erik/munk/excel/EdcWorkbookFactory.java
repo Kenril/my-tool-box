3711068c101690dc15e706e742bc76ad73ebc7f8
Erik Munk-Jakobsen <munk.jakobsen.erik@gmail.com>
2020-03-18 17:25:29 +0100

    Working with Excel file generation

---
+++ java/src/main/java/erik/munk/excel/EdcWorkbookFactory.java
@@ -0,0 +1,62 @@
+/**
+ *
+ */
+package erik.munk.excel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EdcWorkbookFactory {
+
+	private EdcWorkbookFactory() {
+
+	}
+
+	private static final String POINT = ".";
+
+	private static final String UNDERSCORE = "_";
+
+	private static final String REGEX_POUR_POINT = "\\.";
+
+	/**
+	 * Creation du fichier excel depuis une string encodé en Base64 repésentant le fichier template excel
+	 * @param excelEncodedFileBase64
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static File createWorkbookFile(final String fileName, final String excelEncodedFileBase64) throws IOException {
+		byte[] f = Base64.decodeBase64(excelEncodedFileBase64);
+		File excel = new File(fileName);
+		if (!excel.exists()) {
+			excel = File.createTempFile(fileName.split(REGEX_POUR_POINT)[0] + UNDERSCORE, POINT + fileName.split(REGEX_POUR_POINT)[1]);
+			log.info("Created temp template.xls file");
+		}
+		try (OutputStream os = new FileOutputStream(excel)) {
+			os.write(f);
+		}
+		if (excel.isFile() && !excel.exists()) {
+			log.warn("failed to create excel template");
+		}
+		return excel;
+	}
+
+	public static Workbook createWorkbook(final String fileName, final String excelEncodedFileBase64) throws IOException {
+		return WorkbookFactory.create(createWorkbookFile(fileName, excelEncodedFileBase64));
+	}
+
+	public static Workbook createWorkbook(final File excelFile) throws IOException {
+		return WorkbookFactory.create(excelFile);
+	}
+
+}
---
+++ java/src/main/java/erik/munk/excel/ParamKey.java
@@ -0,0 +1,9 @@
+package erik.munk.excel;
+
+public class ParamKey {
+
+
+	public static final String KEY_EDC_EXCEL_CELL_WITH_SIRET = "";
+	public static final String KEY_EDC_EXCEL_INDEX_LINE_END_HEADERS = "";
+	public static final String KEY_EDC_EXCEL_CELL_WITH_START_PERIODE = "";
+}
