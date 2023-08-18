24c53060488b9e26bc880447c164df63d8be8708
Kenril <munk.jakobsen.erik@gmail.com>
2023-08-18 14:37:04 +0200

    Mini change

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,5 +1,4 @@
-# Some good standards, which are not used if the user
-# creates his/her own .bashrc/.bash_profile
+# Some good standards
 
 # --show-control-chars: help showing Korean or accented characters
 alias ls='ls -F --color=auto --show-control-chars'
